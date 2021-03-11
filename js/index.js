f7c6f9177331fe835fc09dd1ab03321ab6a65148
IngeScobedo <63669425+IngeScobedo@users.noreply.github.com>
2021-03-11 16:35:07 -0600

    'DarkMode'

--- js/index.js
+++ js/index.js
@@ -1,9 +1,23 @@
 var facebook = document.getElementsByClassName("card-followers-number");
 var header_p = document.getElementsByClassName("header-p");
-console.log(header_p);
+const checkbox = document.getElementById("checkbox");
+const switch_dark = document.querySelector(".switch");
+console.log(switch_dark);
+
 var total = 0;
 for (const followers of facebook) {
-    total += parseInt(followers.outerText)
+  total += parseInt(followers.outerText);
 }
-header_p[0].innerHTML = `Total Followers: ${total}`;
-console.log(total);
+header_p[0].innerHTML = `Total Followers: ${new Intl.NumberFormat(
+  "en-IN"
+).format(total)}`;
+
+console.log();
+if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+  checkbox.setAttribute("checked", true);
+}
+checkbox.addEventListener("change", function (event) {
+  console.log(this.checked);
+  document.body.classList.toggle("is-dark-mode");
+  
+});

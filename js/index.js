b248aaaad07f00f4545966bdd0298f4e162aaaf1
IngeScobedo <63669425+IngeScobedo@users.noreply.github.com>
2021-05-25 14:16:25 -0500

    Completed

--- js/index.js
+++ js/index.js
@@ -2,7 +2,6 @@ var facebook = document.getElementsByClassName("card-followers-number");
 var header_p = document.getElementsByClassName("header-p");
 const checkbox = document.getElementById("checkbox");
 const switch_dark = document.querySelector(".switch");
-console.log(switch_dark);
 
 var total = 0;
 for (const followers of facebook) {
@@ -12,12 +11,18 @@ header_p[0].innerHTML = `Total Followers: ${new Intl.NumberFormat(
   "en-IN"
 ).format(total)}`;
 
-console.log();
 if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
   checkbox.setAttribute("checked", true);
 }
 checkbox.addEventListener("change", function (event) {
-  console.log(this.checked);
   document.body.classList.toggle("is-dark-mode");
   
 });
+
+let num = 10;
+
+if(num > 5){
+    let num = 25;
+}
+
+num++;
